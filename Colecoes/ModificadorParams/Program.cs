72f7e2fcba5b7d797cb95816ad3107806b6c2574
MuriloRohor <murilo.marquezini@gmail.com>
2023-07-19 10:24:57 -0300

    modificador params

---
+++ Colecoes/ModificadorParams/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("## Modificador params ##");
+
+int[] valores = { 1, 2, 3 };
+
+var resultado = Calcular.Soma(valores);
+Console.WriteLine(resultado);
+
+var resultado2 = Calcular.Soma(1, 2 ,3 ,4);
+Console.WriteLine(resultado2);
+
+
+Console.ReadKey();
+
+public class Calcular
+{
+    public static int Soma( params int[] numeros)
+    {
+        int total = 0;
+        foreach (var numero in numeros)
+        {
+            total = total + numero;
+        }
+        return total;
+    }
+}
