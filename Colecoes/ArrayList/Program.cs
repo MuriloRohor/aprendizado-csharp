7df1ff94d2f6f465f06f47068aed19a893295ded
MuriloRohor <murilo.marquezini@gmail.com>
2023-07-19 22:10:33 -0300

    Aplicando ArrayList

---
+++ Colecoes/ArrayList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+Console.WriteLine("## ArrayList ##");
+
+// Formas de declarar um ArrayList
+
+ArrayList lista;
+lista = new ArrayList();
+
+ArrayList lista2 = new ArrayList();
+var lista3 = new ArrayList();
+ArrayList lista4 = new();
+
+// declarando o Capacity das ArrayList
+
+ArrayList lista5 = new ArrayList(5);
+var lista6 = new ArrayList(5);        // -> Capacity = 5
+ArrayList lista7 = new(5);
+
+// incluir elementos no ArrayList
+
+lista.Add("Paulo");
+lista.Add(4.5);
+lista.Add(5);
+lista.Add(true);           // -> Uma ArrayList permite incluir elementos de diferentes tipos de dados.
+lista.Add(" ");
+lista.Add(null);
+
+// Usando inicializadores de objetos
+
+var lista8 = new ArrayList() { "Paulo", 4.5, 5, true, " ", null };
