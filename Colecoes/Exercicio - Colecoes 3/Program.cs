23aa8c382d779eeece1f37a52e7b593819744947
MuriloRohor <murilo.marquezini@gmail.com>
2023-07-27 08:49:33 -0300

    Exercicio 3

---
+++ Colecoes/Exercicio - Colecoes 3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("## Exercicio Colecoes 3 ##");
+
+// 3 - Escreva um programa em C# que solicite ao usuário que informe o valor das notas, do tipo float , para 10
+// alunos divididos em 2 grupos de 5 alunos cada. Armazene as informações em um array bidimensional e a
+// seguir exiba no console a média aritmética de cada grupo de alunos (grupo1 e grupo2).
