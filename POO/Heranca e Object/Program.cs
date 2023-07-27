358c2b4c2015c78c1cd52e1d40deab8c2e6f55a3
MuriloRohor <murilo.marquezini@gmail.com>
2023-07-27 11:15:30 -0300

    Herança e Object

---
+++ POO/Heranca e Object/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Timers;
+
+Console.WriteLine("## Herança e Object ##");
+
+// Toda a classe é derivada da classe Object, e, a classe Object não deriva de nenhuma classe
+// Ela é a classe Base (Pai) de todas as classes.
+
+// A classe Object dá supore a todas as classes na hierarquia de classes do .NET e fornece serviçoes de baixo nivel
+// para as classes derivadas.
+
+// Os métodos definidos na classe Object estão disponíveis em todos os objetos do sistema e as classes derivadas podem
+// substituir alguns desses méotdos.
+
+// - Equals      : Suporta comparações entre objetos;
+// - GetHashCode : Gera um número hash para o objeto;
+// - ToString    : Retorna a representação do objeto como uma string;
+// - GetType     : Retorna a informação sobre o tipo;
+
+Nada nada = new Nada();
+
+Console.ReadKey();
+public class Nada
+{
+
+}
