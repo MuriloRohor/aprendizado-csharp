77be5a7c821bd2cbed07d8ff5bc0e1d5eea17f55
MuriloRohor <murilo.marquezini@gmail.com>
2023-07-31 22:11:07 -0300

    Agragação

--- POO/Classes - Agregacao/Program.cs
+++ POO/Classes - Agregacao/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Classes - Agragação");
+﻿using Classes___Agregacao;
+
+Console.WriteLine("Classes - Agragação");
 // Um relacionamento de agregação é um tipo especial de composição onde objetos de uma classe
 // podem conter um ou mais objetos de outra classe
 
@@ -6,3 +8,24 @@
 // Representa uma relação Todo - parte
 // - A parte e o todo são independentes
 // - A parte pode existir sem o todo
+
+Professor prof1 = new Professor("Carlos", "Química");
+Professor prof2 = new Professor("Maria", "Inglês");
+Professor prof3 = new Professor("Ana", "Literatura");
+Professor prof4 = new Professor("Paulo", "Matemática");
+
+Departamento dep1 = new Departamento("Exatas");
+
+dep1.IncluirProfesor(prof1);
+dep1.IncluirProfesor(prof4);
+dep1.ListaProfessores();
+
+Console.ReadKey();
+
+Departamento dep2 = new Departamento("Humanas");
+
+dep2.IncluirProfesor(prof2);
+dep2.IncluirProfesor(prof3);
+dep2.ListaProfessores();
+
+Console.ReadKey();
